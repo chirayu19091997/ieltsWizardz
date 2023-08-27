546a5c12bdd0b0f7c35b261ba7ace76e714dc0bf
Jash Agrawal <agrawaljash99@gmail.com>
2023-08-27 23:02:11 +0530

    cleanup duplicate metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,17 +7,6 @@ import "./aboutStyles.css";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  openGraph: {
-    siteName: "IELTS Wizardz",
-    url: "www.ieltswizardz.com",
-    title: "IELTS Wizardz",
-    description:
-      "Explore our study abroad education services, including IELTS, TOEFL, SAT, PTE preparation, document drafting, and admission assistance for international students. Find the perfect programs and universities for your global learning journey.",
-  },
-  title: {
-    template: "%s | IELTS Wizardz",
-    default: "IELTS Wizardz",
-  },
   keywords: [
     "IELTS Wizardz",
     "English language",
