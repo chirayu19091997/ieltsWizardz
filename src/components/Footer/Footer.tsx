065931d4065b6e7016ee8eccc5abd93383a983f0
ieltswizardz <141854767+ieltswizardz@users.noreply.github.com>
2024-02-14 09:15:35 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -96,7 +96,7 @@ const Footer = () => {
         <hr className="mt-16 mb-10 border-gray-200" />
 
         <p className="text-sm text-center text-gray-600">
-          © Copyright {CopyrightYear}, All Rights Reserved by IELTS Wizardz
+          © Copyright {CopyrightYear}, All Rights Reserved by Wizardz Overseas Education
         </p>
       </div>
     </section>
